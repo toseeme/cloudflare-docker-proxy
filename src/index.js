99713b8ab3fffdbabf8f9eed824dbe1fe15ac858
toseeme <40018420+toseeme@users.noreply.github.com>
2024-08-03 19:31:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.be.cn.eu.org": dockerHub,
+  "quay.be.cn.eu.org": "https://quay.io",
+  "gcr.be.cn.eu.org": "https://gcr.io",
+  "k8s-gcr.be.cn.eu.org": "https://k8s.gcr.io",
+  "k8s.be.cn.eu.org": "https://registry.k8s.io",
+  "ghcr.be.cn.eu.org": "https://ghcr.io",
+  "cloudsmith.be.cn.eu.org": "https://docker.cloudsmith.io",
+  "ecr.be.cn.eu.org": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
